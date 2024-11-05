44b10c00c06b57b71d5fd8d0e47695a9c25be873
chrismclarke <chris.m.clarke@live.co.uk>
2024-11-04 20:17:56 -0800

    chore: wip deno migrate scripts

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -1,8 +1,28 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { resolveBinary } from "npm:dbmate";
+import { copy, readerFromStreamReader } from "jsr:@std/io";
+
+/**
+ * WiP - attempt to use dbmate via cli triggered from Deno
+ * This could be useful for generating new migration files during development,
+ * but likely less preferred than calling from docker container image
+ */
+
+if (import.meta.main) {
+  const commonArgs = ["--migrations-dir", "./migrations"];
+  dbMateExec(commonArgs.concat(Deno.args));
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+/**
+ * Execute a cli command with dbmate
+ * https://github.com/amacneil/dbmate?tab=readme-ov-file#commands
+ */
+async function dbMateExec(args: string[]) {
+  const cliPath = resolveBinary();
+  const { stdout, stderr, status } = new Deno.Command(
+    cliPath,
+    { args, stdout: "piped", stderr: "piped" },
+  ).spawn();
+  copy(readerFromStreamReader(stdout.getReader()), Deno.stdout);
+  copy(readerFromStreamReader(stderr.getReader()), Deno.stderr);
+  await status;
 }
